e5d35f7f4ba0a777f9965a30de5e8788d84f26d9
petim7277 <petim7277@gmail.com>
2024-11-30 20:01:44 +0100

    Everything added

--- src/main/java/com/example/KudiSave/repositories/KudiUserRepository.java
+++ src/main/java/com/example/KudiSave/repositories/KudiUserRepository.java
@@ -4,6 +4,7 @@ import com.example.KudiSave.KudiUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public interface KudiUserRepository extends MongoRepository<KudiUser, String> {
 
